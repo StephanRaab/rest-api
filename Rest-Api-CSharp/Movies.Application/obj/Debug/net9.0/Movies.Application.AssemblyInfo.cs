f9d6bde647743e6c7e6dbe18b17ffb2626c3268d
Stephan Raab <sarcartist@gmail.com>
2025-02-24 14:47:22 -0500

    finish creating the temporary db

--- Rest-Api-CSharp/Movies.Application/obj/Debug/net9.0/Movies.Application.AssemblyInfo.cs
+++ Rest-Api-CSharp/Movies.Application/obj/Debug/net9.0/Movies.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Movies.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5037231fdd5e826437d25387616b5d4d0b8e7b8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23c213e291548b756274e0c3d7dc30dcfb74a66f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Movies.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Movies.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
