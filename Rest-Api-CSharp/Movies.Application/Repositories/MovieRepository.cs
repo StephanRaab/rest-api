23c213e291548b756274e0c3d7dc30dcfb74a66f
Stephan Raab <sarcartist@gmail.com>
2025-02-24 14:39:53 -0500

    start add methods for the interface implementation

--- Rest-Api-CSharp/Movies.Application/Repositories/MovieRepository.cs
+++ Rest-Api-CSharp/Movies.Application/Repositories/MovieRepository.cs
@@ -4,28 +4,38 @@ namespace Movies.Application.Repositories;
 
 public class MovieRepository : IMovieRepository
 {
+    private readonly List<Movie> _movies = new List<Movie>();
+    
     public Task<bool> CreateAsync(Movie movie)
     {
-        throw new NotImplementedException();
+        _movies.Add(movie);
+        return Task.FromResult(true);
     }
 
     public Task<Movie?> GetByIdAsync(Guid id)
     {
-        throw new NotImplementedException();
+        var movie = _movies.SingleOrDefault(x => x.Id == id);
+        return Task.FromResult(movie);
     }
 
     public Task<IEnumerable<Movie>> GetAllAsync()
     {
-        throw new NotImplementedException();
+        return Task.FromResult(_movies.AsEnumerable());
     }
 
     public Task<bool> UpdateAsync(Movie movie)
     {
-        throw new NotImplementedException();
+        var movieIndex = _movies.IndexOf(movie);
+        if (movieIndex == -1) return Task.FromResult(false);
+        
+        _movies[movieIndex] = movie;
+        return Task.FromResult(true);
     }
 
     public Task<bool> DeleteByIdAsync(Guid id)
     {
-        throw new NotImplementedException();
+        var removedCount = _movies.RemoveAll(x => x.Id == id);
+        var movieRemoved = removedCount > 0;
+        return Task.FromResult(movieRemoved);
     }
 }
