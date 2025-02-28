ddb663e2abdd9d4236856315bad6200893ff6504
Stephan Raab <sarcartist@gmail.com>
2025-02-28 15:37:09 -0500

    movie retrieval implementation

--- Rest-Api-CSharp/Movies.Api/obj/Debug/net9.0/Movies.Api.AssemblyInfo.cs
+++ Rest-Api-CSharp/Movies.Api/obj/Debug/net9.0/Movies.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Movies.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b67314d30f12a42aeab988e44964adf2a14b31f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+824df4a77b32977d2c1806e916445599d01cd77f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Movies.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Movies.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
